aad05c838d0a502d9949db7cd0cd62f6cbc976b8
Mehak-Conrad <148427140+Mehak-Conrad@users.noreply.github.com>
2024-10-18 15:47:01 +0500

    Feat: Integrate Query Similarity-Based Example Retrieval in Few-Shot Prompts (#16)
    * feat(BasePrompt): load examples based on query similarity
    
    * fix: correct the shots and example if else statement logic

--- server/utilities/prompts/base_prompt.py
+++ server/utilities/prompts/base_prompt.py
@@ -1,27 +1,32 @@
 from abc import ABC, abstractmethod
 import json
-import os
 
-from utilities.config import DatabaseConfig
+from utilities.vectorize import vectorize_data_samples, fetch_few_shots
+from utilities.constants.response_messages import ERROR_FETCHING_EXAMPLES, ERROR_SCHEMA_FILE_NOT_FOUND
 
 class BasePrompt(ABC):
-    def __init__(self, examples=None, target_question=None, shots=None):
-        if shots and shots > 0:
-            # finding similarity between example will be here
-
-            file_path = os.path.join(
-                os.path.abspath(os.path.dirname(__file__)),
-                f'../../data/sample_questions_and_queries/{DatabaseConfig.ACTIVE_DATABASE.value}_schema.json' 
-            )
-
-            with open(file_path, 'r') as file:
-                all_samples = json.load(file)
-                self.examples= all_samples[:shots]
-        else:
-            self.examples= examples
-
-        self.shots = shots
+    def __init__(self, examples = None, target_question = None, shots = 0):
         self.target_question = target_question
+        self.shots = shots
+
+        if not self.shots or self.shots <= 0:
+            self.examples = None
+            return
+            
+        if examples and len(examples) > 0:
+            self.examples = examples[:self.shots] if self.shots <= len(examples) else examples
+            return
+        
+        self.examples = self.fetch_examples_based_on_query_similarity()
+        
+    def fetch_examples_based_on_query_similarity(self):
+        try:
+            vectorize_data_samples()
+            return fetch_few_shots(self.shots, self.target_question)
+        except FileNotFoundError as e:
+            raise FileNotFoundError(ERROR_SCHEMA_FILE_NOT_FOUND.format(error=str(e)))
+        except Exception as e:
+            raise RuntimeError(ERROR_FETCHING_EXAMPLES.format(error=str(e)))
 
     @abstractmethod
     def get_prompt(self) -> str:

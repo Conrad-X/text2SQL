42259e0df185e331320907fbdb105d1bd4212ebe
Mehak Farhan <mehak.farhan@conradlabs.com>
2024-10-28 16:12:28 +0500

    feat: Initial Implementation of OpenAI Batch Jobs - Implemented a code solution for creating and running batch jobs in the codebase.

--- server/services/openai_client.py
+++ server/services/openai_client.py
@@ -1,21 +1,37 @@
 
 from openai import OpenAI
+import os
+from typing import Optional
 
-from utilities.config import OPENAI_API_KEY
+from utilities.config import (
+    OPENAI_API_KEY, 
+    BATCH_OUTPUT_FILE_DIR, 
+    BATCH_INPUT_FILE_DIR, 
+    BATCH_INPUT_FILE_NAME,
+    BATCH_OUTPUT_FILE_NAME
+)
 from utilities.constants.LLM_enums import LLMType, ModelType
-from utilities.constants.response_messages import ERROR_API_FAILURE
+from utilities.constants.response_messages import (
+    ERROR_API_FAILURE, 
+    ERROR_BATCH_JOB_CREATION, 
+    ERROR_UPLOAD_BATCH_INPUT_FILE,
+    ERROR_GET_ALL_BATCHES, 
+    ERROR_GET_ALL_UPLOADED_FILES, 
+    ERROR_DOWNLOAD_BATCH_FILE,
+    ERROR_BATCH_INPUT_FILE_NOT_FOUND
+)
 
 from services.base_client import Client 
 
 class OpenAIClient(Client):
-    def __init__(self, model: ModelType, max_tokens: int, temperature: float):
+    def __init__(self, model: Optional[ModelType], max_tokens: Optional[int], temperature: Optional[float]):
         client = OpenAI(api_key=OPENAI_API_KEY)
-        super().__init__(model=model.value, temperature=temperature, max_tokens=max_tokens, client=client)
+        super().__init__(model=model, temperature=temperature, max_tokens=max_tokens, client=client)
 
     def execute_prompt(self, prompt: str) -> str:
         try:
             response = self.client.chat.completions.create(
-                model=self.model, 
+                model=self.model.value, 
                 messages=[{"role": "user", "content": prompt}],
                 max_tokens=self.max_tokens,
                 temperature=self.temperature,
@@ -28,6 +44,54 @@ class OpenAIClient(Client):
 
         except Exception as e:
             raise RuntimeError(ERROR_API_FAILURE.format(llm_type=LLMType.OPENAI.value, error=str(e)))
+        
+    def upload_batch_input_file(self, database_name: str) -> str:
+        file_path = f"{BATCH_INPUT_FILE_DIR}/{BATCH_INPUT_FILE_NAME.format(database_name=database_name)}"
+
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(ERROR_BATCH_INPUT_FILE_NOT_FOUND.format(file_name={BATCH_INPUT_FILE_NAME.format(database_name=database_name)}))
+        try:
+            with open(file_path, "rb") as file:
+                batch_input_file = self.client.files.create(file=file, purpose="batch")
+            return batch_input_file
+        except Exception as e:
+            raise RuntimeError(ERROR_UPLOAD_BATCH_INPUT_FILE.format(error=str(e)))
+
+    def create_batch_job(self, file_id: str) -> str:
+        try:
+            batch = self.client.batches.create(
+                input_file_id=file_id,
+                endpoint="/v1/chat/completions",
+                completion_window="24h",
+            )
+            return batch
+        except Exception as e:
+            raise RuntimeError(ERROR_BATCH_JOB_CREATION.format(error=str(e)))
+        
+    def get_all_batches(self):
+        try:
+            return self.client.batches.list()
+        except Exception as e:
+            raise RuntimeError(ERROR_GET_ALL_BATCHES.format(error=str(e)))
+        
+    def get_all_uploaded_files(self):
+        try:
+            return self.client.files.list()
+        except Exception as e:
+            raise RuntimeError(ERROR_GET_ALL_UPLOADED_FILES.format(error=str(e)))
+
+        
+    def download_file(self, file_id: str, database_name: str):
+        try:
+            file_content = self.client.files.content(file_id)
+            with open(f"{BATCH_OUTPUT_FILE_DIR}/{BATCH_OUTPUT_FILE_NAME.format(database_name=database_name)}", "w") as f:
+                f.write(file_content.text)
+            
+            return file_content.text
+        except Exception as e:
+            raise RuntimeError(ERROR_DOWNLOAD_BATCH_FILE.format(error=str(e)))
+        
+
 
 
 

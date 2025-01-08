6e52ca09b2e1520f24195ef24872c3ffbdc4a23a
Mehak-Conrad <148427140+Mehak-Conrad@users.noreply.github.com>
2025-01-08 15:43:51 +0500

    Feat: Add Google AI Client and Models (#34)
    * feat: add google ai as client and add it's respective models
    
    * fix: add GoogleAI client to  client_factory.py

--- server/services/client_factory.py
+++ server/services/client_factory.py
@@ -3,6 +3,7 @@ from typing import Optional
 from services.base_client import Client 
 from services.openai_client import OpenAIClient
 from services.anthropic_client import AnthropicClient
+from services.google_ai_client import GoogleAIClient
 
 from utilities.constants.LLM_enums import LLMType, ModelType
 from utilities.constants.response_messages import ERROR_UNSUPPORTED_CLIENT_TYPE
@@ -14,5 +15,7 @@ class ClientFactory:
             return OpenAIClient(model=model, temperature=temperature, max_tokens=max_tokens)
         elif type == LLMType.ANTHROPIC:
             return AnthropicClient(model=model, temperature=temperature, max_tokens=max_tokens)
+        elif type == LLMType.GOOGLE_AI:
+            return GoogleAIClient(model=model, temperature=temperature, max_tokens=max_tokens)
         else:
             raise ValueError(ERROR_UNSUPPORTED_CLIENT_TYPE)

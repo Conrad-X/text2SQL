354d6f7d7aaf1d7bb070fe29031e2f8b9da7458b
Mehak Farhan <mehak.farhan@conradlabs.com>
2025-02-06 15:15:57 +0500

    feat(schema-pruning): add option to extract keywords using a prompt

--- server/utilities/schema_linking/extract_keyword.py
+++ server/utilities/schema_linking/extract_keyword.py
@@ -1,36 +1,62 @@
+import time
 import datefinder
-import re
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
 from nltk.chunk import ne_chunk
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet
 
-# TO DO: Add Evidence/Hint from BIRD and figure out how to extract keywords if Evidence has part of SQL
+from utilities.logging_utils import setup_logger
+from utilities.constants.script_constants import GOOGLE_RESOURCE_EXHAUSTED_EXCEPTION_STR
+from services.client_factory import ClientFactory
+from services.base_client import Client
+from utilities.constants.LLM_enums import LLMType, ModelType
+from utilities.prompts.prompt_templates import EXTRACT_KEYWORD_PROMPT_TEMPLATE
+from utilities.constants.response_messages import UNKNOWN_ERROR
+
+logger = setup_logger(__name__)
+
 
 def get_date_keywords(text: str):
-    # Use datefinder to locate dates in the text
+    """
+    Extracts date keywords from the given text.
+    """
+
     matches = list(datefinder.find_dates(text, source=True))
     date_entities = []
     for _, date_str in matches:
         # date_str contains the substring that was recognized as a date
         start_index = text.find(date_str)
         if start_index != -1:
-            date_entities.append({
-                "entity": "DATE",
-                "word": date_str,
-                "start": start_index,
-                "end": start_index + len(date_str)
-            })
+            date_entities.append(
+                {
+                    "entity": "DATE",
+                    "word": date_str,
+                    "start": start_index,
+                    "end": start_index + len(date_str),
+                }
+            )
 
-    return [entity['word'] for entity in date_entities]
+    return [entity["word"] for entity in date_entities]
 
-def convert_word_to_singular_form(word):
+
+def convert_word_to_singular_form(word: str):
+    """
+    Converts a word to its singular form using wordnet
+    """
+
     singular_word = wordnet.morphy(word)
     return singular_word if singular_word else word
 
-def get_keywords_from_question(question: str):
+
+def get_keywords_from_question(question: str, evidence: str):
+    """
+    Extracts keywords using NLTK from the given question and evidence.
+    """
+
     tokens = word_tokenize(question)
+    tokens = tokens + word_tokenize(evidence) if evidence else tokens
+
     pos_tags = pos_tag(tokens)
 
     # Perform Named Entity Recognition (NER)
@@ -39,11 +65,17 @@ def get_keywords_from_question(question: str):
 
     # Extract named entities like 'United States', 'Acme Corp'
     for chunk in chunks:
-        if hasattr(chunk, 'label') and chunk.label() in ["GPE", "PERSON", "ORGANIZATION"]:
+        if hasattr(chunk, "label") and chunk.label() in [
+            "GPE",
+            "PERSON",
+            "ORGANIZATION",
+        ]:
             named_entities.append(" ".join(c[0] for c in chunk))
 
     # Extract all potential keywords based on POS tags (nouns and adjectives are usually key terms)
-    possible_keywords = [word for word, tag in pos_tags if tag in ['NN', 'NNS', 'NNP', 'NNPS', 'JJ']]
+    possible_keywords = [
+        word for word, tag in pos_tags if tag in ["NN", "NNS", "NNP", "NNPS", "JJ"]
+    ]
 
     # Extract Date elements from the question (e.g., '8/10/2009')
     date_keywords = get_date_keywords(question)
@@ -52,7 +84,46 @@ def get_keywords_from_question(question: str):
     all_keywords = set(named_entities + possible_keywords + date_keywords)
 
     # Dynamically filter out stopwords early
-    stop_words = set(stopwords.words('english'))
-    filtered_keywords = set([convert_word_to_singular_form(word) for word in all_keywords if word.lower() not in stop_words])
+    stop_words = set(stopwords.words("english"))
+    filtered_keywords = set(
+        [
+            convert_word_to_singular_form(word)
+            for word in all_keywords
+            if word.lower() not in stop_words
+        ]
+    )
 
     return list(filtered_keywords)
+
+
+def get_keywords_using_LLM(
+    question: str,
+    evidence: str,
+    LLMType: LLMType,
+    ModelType: ModelType,
+    temperature: float,
+    max_tokens: int,
+):
+    """
+    Extracts keywords from the given question and evidence using the specified LLM model.
+    """
+
+    keywords = None
+
+    client = ClientFactory.get_client(LLMType, ModelType, temperature, max_tokens)
+    prompt = EXTRACT_KEYWORD_PROMPT_TEMPLATE.format(
+        question=question, hint=evidence if evidence else ""
+    )
+
+    while keywords == None:
+        try:
+            keywords = client.execute_prompt(prompt=prompt)
+            keywords = keywords.replace("```python", "").replace("```", "").strip()
+        except Exception as e:
+            if GOOGLE_RESOURCE_EXHAUSTED_EXCEPTION_STR in str(e):
+                # Wait for 5 seconds before retrying
+                time.sleep(5)
+            else:
+                logger.error(UNKNOWN_ERROR.format(e))
+
+    return keywords
--- server/utilities/schema_linking/schema_linking_utils.py
+++ server/utilities/schema_linking/schema_linking_utils.py
@@ -1,20 +1,43 @@
 from collections import defaultdict
 from utilities.vectorize import fetch_similar_columns
-from utilities.schema_linking.extract_keyword import get_keywords_from_question
+from utilities.schema_linking.extract_keyword import (
+    get_keywords_from_question,
+    get_keywords_using_LLM,
+)
 from utilities.schema_linking.value_retrieval import get_table_column_of_similar_values
+from utilities.constants.LLM_enums import LLMType, ModelType
 
-def get_relevant_tables_and_columns(query: str, n_description: int, n_value: int, database_name: str) -> dict:
+
+def get_relevant_tables_and_columns(
+    query: str,
+    evidence: str,
+    n_description: int,
+    n_value: int,
+    database_name: str,
+    use_llm: bool,
+    LLMType: LLMType = None,
+    ModelType: ModelType = None,
+    temperature: float = 0.7,
+    max_tokens: int = 8000,
+) -> dict:
     """
     This function takes a str as input and returns a dictionary of relevant tables and columns from the current Database.
     """
-    keywords = get_keywords_from_question(query)
+
+    if use_llm:
+        keywords = get_keywords_using_LLM(
+            query, evidence, LLMType, ModelType, temperature, max_tokens
+        )
+    else:
+        keywords = get_keywords_from_question(query, evidence)
+
     schema = {}
     similar_columns = {}
     value_columns = {}
 
     similar_columns = fetch_similar_columns(n_description, keywords, database_name)
     value_columns = get_table_column_of_similar_values(keywords, n_value, database_name)
-    
+
     for table, columns in similar_columns.items():
         if table not in schema:
             schema[table] = set()

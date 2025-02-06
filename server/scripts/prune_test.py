bb9355fc3d698df4ca3a2c01f9a39c38504e6b0f
fi5421 <firas.abdullah@conradlabs.com>
2025-02-06 14:12:48 +0500

    chore: resolving PR comments

--- server/scripts/prune_test.py
+++ server/scripts/prune_test.py
@@ -53,42 +53,29 @@ def compare_dicts(dict1, dict2):
     total_score = (0.5 * key_score) + (0.5 * value_score)  # Weight keys & values equally
     return total_score
 
-if __name__ == '__main__':
+def process_databases():
 
     directories = [d for d in os.listdir(DATASET_DIR) if os.path.isdir(os.path.join(DATASET_DIR, d))]
 
     score_dict = {'database':[], 'prune_score':[], 'num_queries':[], 'set':[]}
     for database in directories:
+        
+        for file_format in [[UNMASKED_SAMPLE_DATA_FILE_PATH, "train"], [TEST_DATA_FILE_PATH, 'test']]:
 
-        with open(UNMASKED_SAMPLE_DATA_FILE_PATH.format(database_name=database)) as file:
-            train_data=json.load(file)
-            file.close()
-        score=0
-        for item in train_data:
-            true_schema = item['schema_used']
-            run_schema = item['runtime_schema_used']
-            score+=compare_dicts(true_schema, run_schema)
+            with open(file_format[0].format(database_name=database)) as file:
+                file_data=json.load(file)
+                file.close()
+            score=0
+            for item in file_data:
+                true_schema = item['schema_used']
+                run_schema = item['runtime_schema_used']
+                score+=compare_dicts(true_schema, run_schema)
 
-        avg_score=score/len(train_data)
-        score_dict['database'].append(database)
-        score_dict['prune_score'].append(avg_score)
-        score_dict['num_queries'].append(len(train_data))
-        score_dict['set'].append('train')
-
-        with open(TEST_DATA_FILE_PATH.format(database_name=database)) as file:
-            test_data=json.load(file)
-            file.close()
-        score=0
-        for item in test_data:
-            true_schema = item['schema_used']
-            run_schema = item['runtime_schema_used']
-            score+=compare_dicts(true_schema, run_schema)
-
-        avg_score=score/len(test_data)
-        score_dict['database'].append(database)
-        score_dict['prune_score'].append(avg_score)
-        score_dict['num_queries'].append(len(test_data))
-        score_dict['set'].append('test')
+            avg_score=score/len(file_data)
+            score_dict['database'].append(database)
+            score_dict['prune_score'].append(avg_score)
+            score_dict['num_queries'].append(len(file_data))
+            score_dict['set'].append(file_format[1])
 
     for split in ['train','test']:
         with open(f"{DATASET_DIR}/processed_{split}.json") as file:
@@ -108,4 +95,25 @@ if __name__ == '__main__':
         score_dict['set'].append(split)
 
     score_df=pd.DataFrame(score_dict)
-    print(score_df)
+    return score_df
+
+if __name__ == '__main__':
+    """
+    Main execution block:
+    
+    - Processes multiple databases by computing schema similarity scores.
+    - Iterates through dataset directories and evaluates query schema consistency.
+    - Computes an average pruning score based on schema similarity.
+    - Aggregates results for both training and testing sets.
+    - Outputs a DataFrame summarizing the pruning scores and query counts.
+
+    The computed DataFrame contains the following columns:
+        - 'database': Name of the database.
+        - 'prune_score': Average similarity score between expected and runtime schemas.
+        - 'num_queries': Number of queries processed for the database.
+        - 'set': Indicates whether the data belongs to 'train' or 'test'.
+
+    The final DataFrame is printed to the console.
+    """
+    
+    print(process_databases())

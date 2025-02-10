f24ced8793d87f712d62b24f7f1c3b489e71c3ea
fi5421 <firas.abdullah@conradlabs.com>
2025-02-10 14:24:06 +0500

    chore: refactor

--- server/scripts/prune_test.py
+++ server/scripts/prune_test.py
@@ -57,44 +57,40 @@ def compare_dicts(dict1, dict2):
     total_score = (0.5 * key_score) + (0.5 * value_score)  # Weight keys & values equally
     return total_score
 
+def score_schema(questions, score_dict):
+    score = 0
+    for item in questions:
+        true_schema = item['schema_used']
+        run_schema = item['runtime_schema_used']
+        score+=compare_dicts(true_schema, run_schema)
+
+    avg_score = score/len(questions)
+    score_dict['database'].append("ALL")
+    score_dict['prune_score'].append(avg_score)
+    score_dict['num_queries'].append(len(questions))
+    score_dict['set'].append('test')
+
+    return score_dict
+
 def process_databases(single_file = False):
 
     directories = [d for d in os.listdir(DATASET_DIR) if os.path.isdir(os.path.join(DATASET_DIR, d))]
 
     score_dict = {'database':[], 'prune_score':[], 'num_queries':[], 'set':[]}
-    for database in directories:
-        
-        with open(TEST_DATA_FILE_PATH.format(database_name=database)) as file:
-            file_data=json.load(file)
-            file.close()
-        score=0
-        for item in file_data:
-            true_schema = item['schema_used']
-            run_schema = item['runtime_schema_used']
-            score+=compare_dicts(true_schema, run_schema)
-
-        avg_score=score/len(file_data)
-        score_dict['database'].append(database)
-        score_dict['prune_score'].append(avg_score)
-        score_dict['num_queries'].append(len(file_data))
-        score_dict['set'].append('test')
-
     if single_file:
         with open(f"{DATASET_DIR}/processed_test.json") as file:
             questions = json.load(file)
             file.close()
 
-        score = 0
-        for item in questions:
-            true_schema = item['schema_used']
-            run_schema = item['runtime_schema_used']
-            score+=compare_dicts(true_schema, run_schema)
-
-        avg_score = score/len(questions)
-        score_dict['database'].append("ALL")
-        score_dict['prune_score'].append(avg_score)
-        score_dict['num_queries'].append(len(questions))
-        score_dict['set'].append('test')
+        score_dict = score_schema(questions, score_dict)
+        
+    else:  
+        for database in directories:
+            
+            with open(TEST_DATA_FILE_PATH.format(database_name=database)) as file:
+                file_data=json.load(file)
+                file.close()
+            score_dict =  score_schema(file_data, score_dict)
 
     score_df=pd.DataFrame(score_dict)
     return score_df

6034777f6bd4371f9ed09909c68e7863cfac6f06
fi5421 <firas.abdullah@conradlabs.com>
2025-03-03 15:18:06 +0500

    chore: merged with updated improver

--- server/scripts/refiner_bench.py
+++ server/scripts/refiner_bench.py
@@ -1,6 +1,6 @@
 from utilities.config import PATH_CONFIG
 import json
-from utilities.sql_improvement import improve_sql_query_chat
+from utilities.sql_improvement import improve_sql_query
 from utilities.logging_utils import setup_logger
 import pandas as pd
 from utilities.constants.LLM_enums import LLMType, ModelType
@@ -10,6 +10,7 @@ import os
 import threading
 import concurrent.futures
 from utilities.utility_functions import execute_sql_timeout
+from utilities.constants.prompts_enums import RefinerPromptType
 
 logger = setup_logger(__name__)
 
@@ -38,7 +39,7 @@ def save_df(data_dict, file_path):
 
 logger = setup_logger(__name__)
 
-def process_question(item, client, refiner_dict, cache, cache_file, refiner_data_file, lock):
+def process_question(item, client, refiner_dict, cache, cache_file, refiner_data_file, lock, prompt_type, shots, max_attempts ):
     """Process a single question in a thread-safe manner."""
     question_id = item['question_id']
     sql = item['sql_gen']
@@ -82,14 +83,14 @@ def process_question(item, client, refiner_dict, cache, cache_file, refiner_data
         logger.error(f"Failed to execute SQL query for {database}: {e}")
         
     # Improve SQL query
-    sql = improve_sql_query_chat(
+    sql = improve_sql_query(
         sql=sql,
-        max_improve_sql_attempts=5,
+        max_improve_sql_attempts=max_attempts,
         database_name=database,
         client=client,
         target_question=test_question['question'],
-        shots=5,
-        prompt_type='xiyan',
+        shots=shots,
+        refiner_prompt_type=prompt_type,
         schema_used=test_question['schema_used'],
         evidence=test_question['evidence'],
     )
@@ -124,12 +125,12 @@ if __name__ == '__main__':
     This file should be cleared before running a new benchmark to ensure that only the latest results are stored.
     """
 
-    max_improve_attempts = 5
+    max_attempts = 5
     client = [LLMType.GOOGLE_AI, ModelType.GOOGLEAI_GEMINI_2_0_FLASH]
     temperature =0.7
     max_tokens = 1024
     shots = 5
-    prompt_type = 'xiyan'
+    prompt_type = RefinerPromptType.XIYAN
 
 
     dataset_dir = PATH_CONFIG.dataset_dir()
@@ -137,7 +138,7 @@ if __name__ == '__main__':
 
     # Load refiner data
     with open(PATH_CONFIG.base_dir() / 'refiner_bench_data.json', 'r') as file:
-        refiner_data = json.load(file)[:10]
+        refiner_data = json.load(file)
 
     # Load cache
     cache_file = PATH_CONFIG.base_dir() / 'refiner_bench_cache.txt'
@@ -162,7 +163,7 @@ if __name__ == '__main__':
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         futures = [
             executor.submit(process_question, item, ClientFactory.get_client(type=client[0], model=client[1], temperature=temperature, max_tokens=max_tokens),
-                            refiner_dict, cache, cache_file, result_file, lock)
+                            refiner_dict, cache, cache_file, result_file, lock, prompt_type, shots, max_attempts)
             for item in refiner_data
         ]
 

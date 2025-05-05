a5bcdfa014c549dfda396cce592b41dae6659f5b
fi5421 <firas.abdullah@conradlabs.com>
2025-05-05 16:04:12 +0500

    refactor: refactored add_descriptions_bird_dataset:  - moved all response messages to utilities/constants/preprocess/add_descriptions_bird_dataset/response_messages.py  - moved all strings as constants to utilities/constants/preprocess/add_descriptions_bird_dataset/constants.py  - moved all sql commands to constants at top of file  - refactored all functions to lower complexity
    also added function in path_config for table_description_file for the tables description csv

---
+++ server/utilities/constants/preprocess/add_descriptions_bird_dataset/constants.py
@@ -0,0 +1,7 @@
+TABLE_DESCRIPTION_PLACEHOLDER = "No Description Available"
+TABLE_NAME_COL = 'table_name'
+TABLE_DESCRIPTION_COL = 'table_description'
+ORIG_COLUMN_NAME_COL = 'original_column_name'
+DATA_FORMAT_COL = "data_format"
+COLUMN_DESCRIPTION_COL = "column_description"
+IMPROVED_COLUMN_DESCRIPTIONS_COL = "improved_column_description"

652ac49d87ed313b42d16d1395fd1eb518948f46
fi5421 <firas.abdullah@conradlabs.com>
2025-05-07 17:08:49 +0500

    chore: Resolving PR comments updated constants file to only contain indexing strings for description csv dataframes

--- server/utilities/constants/preprocess/add_descriptions_bird_dataset/indexing_constants.py
+++ server/utilities/constants/preprocess/add_descriptions_bird_dataset/indexing_constants.py
@@ -1,8 +1,6 @@
-TABLE_DESCRIPTION_PLACEHOLDER = "No Description Available"
 TABLE_NAME_COL = 'table_name'
 TABLE_DESCRIPTION_COL = 'table_description'
 ORIG_COLUMN_NAME_COL = 'original_column_name'
 DATA_FORMAT_COL = "data_format"
 COLUMN_DESCRIPTION_COL = "column_description"
 IMPROVED_COLUMN_DESCRIPTIONS_COL = "improved_column_description"
-TABLE_DESCRIPION_FILE = "{database_name}_tables.csv"

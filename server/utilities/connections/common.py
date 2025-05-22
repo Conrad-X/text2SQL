0a5c46b3ffc28c8c1c59bd949ece1923f7e96773
Awais Kamran <awais.kamran@hotmail.com>
2025-05-22 20:29:18 +0500

    fix: close_connection method removed

--- server/utilities/connections/common.py
+++ server/utilities/connections/common.py
@@ -5,14 +5,3 @@ This module provides functions for managing database connections
 such as closing connections safely.
 """
 
-def close_connection(connection):
-    """Close a database connection if it exists.
-
-    Safely closes the provided database connection if it is not None.
-
-    Args:
-        connection : object
-            The database connection object to be closed.
-    """
-    if connection:
-        connection.close()
